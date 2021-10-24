812f56279f7a3b861a5df45d78a4ad95a722d659
Marcelo Miyachi <marcelo.m1811@gmail.com>
2021-10-24 15:15:00 -0300

    feat(newPost): add handleChangeInputs

--- src/pages/auth/LoginPage/LoginPage.stories.tsx
+++ src/pages/auth/LoginPage/LoginPage.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import LoginPage from 'pages/auth/LoginPage';
+import LoginPage from './';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
--- src/pages/posts/NewPostPage/index.tsx
+++ src/pages/posts/NewPostPage/index.tsx
@@ -6,17 +6,43 @@ import { Select } from "@chakra-ui/select";
 import { Textarea } from "@chakra-ui/textarea";
 import BaseFormControl from "components/BaseFormControl";
 import { DefaultLayout } from "layouts/DefaultLayout";
+import { useState } from "react";
+
+enum fieldNames {
+  Content = 'content',
+  Subject = 'subject',
+  Photo = 'photo'
+};
 
 function NewPostPage() {
+  const [content, setContent] = useState<string>('');
+  const [subject, setSubject] = useState<string>('');
+  const [photo, setPhoto] = useState<string>('');
+  
+  const handleChangeInput = (value: any, field: string) => {
+    const functionByField: any = {
+      [fieldNames.Content]: () => setContent(value),
+      [fieldNames.Subject]: () => setSubject(value),
+    }
+    
+    functionByField[field]();
+  }
+
   return (
     <Container>
       <Heading>Criar novo post</Heading>
       <BaseFormControl label='Conteúdo'>
-        <Textarea placeholder="Compartilhe o que você está pensando" />
+        <Textarea placeholder="Compartilhe o que você está pensando"
+          onChange={(e) => handleChangeInput(e.target.value, fieldNames.Content)}
+        />
       </BaseFormControl>
 
-      <BaseFormControl label='Assunto'>
-        <Select placeholder="Selecione um assunto">
+      <BaseFormControl
+        label='Assunto'>
+        <Select
+          placeholder="Selecione um assunto" 
+          onChange={(e) => handleChangeInput(e.target.value, fieldNames.Subject)}
+        >
           <option>Tecnologia</option>
           <option>Marketing</option>
         </Select>
